9204f76ab2a890a9d349d1ac7b1b69a2cf0c85d7
Nikita Chotii <chotii.nikita@yoctocode.com>
2018-12-06 17:24:37 +0200

    Updates

---
+++ precache-manifest.a1ed7226a7f0cb5ed1be470d68fa0bfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd883593b5a1d891c24f523e6e977b71",
+    "url": "/smpl-react-weather-app/static/media/bg.cd883593.jpg"
+  },
+  {
+    "revision": "9c55a8c0df429b9e0121",
+    "url": "/smpl-react-weather-app/static/js/runtime~main.9c55a8c0.js"
+  },
+  {
+    "revision": "48e7fdf4f7ad9d5d9ea3",
+    "url": "/smpl-react-weather-app/static/js/main.48e7fdf4.chunk.js"
+  },
+  {
+    "revision": "c3b345d637d6fe82d125",
+    "url": "/smpl-react-weather-app/static/js/1.c3b345d6.chunk.js"
+  },
+  {
+    "revision": "48e7fdf4f7ad9d5d9ea3",
+    "url": "/smpl-react-weather-app/static/css/main.5f310af0.chunk.css"
+  },
+  {
+    "revision": "c3b345d637d6fe82d125",
+    "url": "/smpl-react-weather-app/static/css/1.8eb6a7fc.chunk.css"
+  },
+  {
+    "revision": "d8fa6ae83038eeece43af53a0a6b50ab",
+    "url": "/smpl-react-weather-app/index.html"
+  }
+];
